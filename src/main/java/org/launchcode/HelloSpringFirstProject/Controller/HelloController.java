b6e7ce681dfbf026bdb4de3402ef0f61a973af49
divya <divyapancholi1995@gmail.com>
2021-06-13 15:24:12 -0500

    done with class annotation initial example.

--- src/main/java/org/launchcode/HelloSpringFirstProject/Controller/HelloController.java
+++ src/main/java/org/launchcode/HelloSpringFirstProject/Controller/HelloController.java
@@ -4,39 +4,38 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@ResponseBody
+@RequestMapping("hello")
 public class HelloController {
 
-    @ResponseBody
-    @GetMapping("hello")
     public String hello(){
         return "Hello, Spring!";
     }
 
-
-    @ResponseBody
     //@GetMapping("goodbye")
+    //method lives at hello/goodbye
     @RequestMapping(value = "goodbye",method = {RequestMethod.GET,RequestMethod.POST})
     public String goodBye(){
         return "Good Bye,Spring";
     }
 
 
-    @ResponseBody
+    //method lives at hello/hello1?name=anything
     @RequestMapping(value="hello1",method = {RequestMethod.GET,RequestMethod.POST})
     public String helloWithQueryParam(@RequestParam  String name){
         return "hello " + name + "!";
     }
 
     //example for path parameter.
-    @ResponseBody
-    @GetMapping("hello/{name}")
+    //method lives at hello/name(name would be anything)
+    @GetMapping("{name}")
     public String helloWithPathParam(@PathVariable String name){
         return "hello " + name + "!";
     }
 
     //form
+    //method lives at hello/form
     @GetMapping("form")
-    @ResponseBody
     public String helloWithForms(){
         return "<html>" +
                 "<body>"+
