4a1e694344fdbc42e2bca595de698c6ff5937d22
Divyapancholi831 <divyapancholi1995@gmail.com>
2021-06-19 16:37:53 -0500

    done with iterate example in static view branch

--- src/main/java/org/launchcode/HelloSpringFirstProject/Controller/HelloController.java
+++ src/main/java/org/launchcode/HelloSpringFirstProject/Controller/HelloController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class HelloController {
 
@@ -51,5 +54,17 @@ public class HelloController {
     @GetMapping("form")
     public String helloWithForms(){
         return "form";
+    }
+
+    @GetMapping("hello-names")
+    public String helloNames(Model model){
+        List<String> names = new ArrayList<>();
+        names.add("java");
+        names.add("javascript");
+        names.add("php");
+        names.add("asp.net");
+        names.add("launchcode");
+        model.addAttribute("names",names);
+        return "hello-list";
     }
 }
